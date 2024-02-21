9067832cb210220f99056e52dd121e794e388356
Omar Rizwan <omar@omar.website>
2024-02-21 18:34:13 -0500

    Print image dims

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from PIL import Image
 import imagehash
 import pickle
 
+import subprocess
+def dims(impath):
+    return subprocess.check_output(['identify', '-format', '%wx%h', impath])
+
 # Load db if it exists. If not, generate it from fb inbox.
 if os.path.isfile("db.pickle"):
     with open("db.pickle", "rb") as f:
@@ -21,16 +25,16 @@ else:
 
 if len(sys.argv) == 2:
     print("Search image:")
-    print(sys.argv[1])
+    print(sys.argv[1], dims(sys.argv[1]))
     imhash = imagehash.phash(Image.open(sys.argv[1]))
     print(imhash)
 
     # sort db by closeness to hash
     db_by_closeness = sorted(db, key=lambda im: im[1] - imhash)
     print("Most similar:")
-    print(db_by_closeness[0][0])
-    print(db_by_closeness[1][0])
-    print(db_by_closeness[2][0])
+    print(db_by_closeness[0][0], dims(db_by_closeness[0][0]))
+    print(db_by_closeness[1][0], dims(db_by_closeness[1][0]))
+    print(db_by_closeness[2][0], dims(db_by_closeness[2][0]))
 
     print("Least similar:")
     print(db_by_closeness[-1][0])
@@ -38,4 +42,3 @@ if len(sys.argv) == 2:
 
     exit(0)
 
-# TODO: Emit a single image of half-width that we can look at.
